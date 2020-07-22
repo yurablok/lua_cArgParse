996455ad8a0d2d0ec8c46bb5c99cfd08466a00e7
yurablok <yurablock@gmail.com>
2020-07-22 11:43:44 +0300

    More versatile Lua includes.

--- lua_cArgParse.hpp
+++ lua_cArgParse.hpp
@@ -39,9 +39,9 @@
 #include <map>
 
 extern "C" {
-#   include "lua/lua.h"
-#   include "lua/lualib.h"
-#   include "lua/lauxlib.h"
+#   include "lua.h"
+#   include "lualib.h"
+#   include "lauxlib.h"
 }
 
 namespace utils::lua {
